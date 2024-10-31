075372d1a6d3f69a4a36ceac9ebe44f3e01d0189
lemon-lime-honey <bs0308@g.skku.edu>
2024-10-31 16:13:00 +0900

    ✨ add example 1-1

---
+++ 01/ex1-1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using OpenCvSharp;
+
+namespace ex1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Cv2.GetVersionString());
+        }
+    }
+}
