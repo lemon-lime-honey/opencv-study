afb9459763c4391ec47dd3ba22ceef7fe0bdfe8e
lemon-lime-honey <bs0308@g.skku.edu>
2024-11-01 11:41:31 +0900

    :sparkles: add example 3-8

---
+++ 03/ex3-8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OpenCvSharp;
+
+namespace ex3_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RotatedRect rotatedRect = new RotatedRect(new Point2f(100f, 100f), new Size2f(100, 100), 45f);
+
+            Console.WriteLine(rotatedRect.BoundingRect());
+            Console.WriteLine(rotatedRect.Points().Length);
+            Console.WriteLine(rotatedRect.Points()[0]);
+        }
+    }
+}
