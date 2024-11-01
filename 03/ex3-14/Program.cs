c14462b961dc56a6460d7f4a091e6aed18468077
lemon-lime-honey <bs0308@g.skku.edu>
2024-11-01 13:50:54 +0900

    :sparkles: add example 3-14

---
+++ 03/ex3-14/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenCvSharp;
+
+namespace ex3_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<int> sizes = new List<int>() { 480, 640 };
+            Mat m = new Mat(sizes, MatType.CV_8UC3);
+        }
+    }
+}
