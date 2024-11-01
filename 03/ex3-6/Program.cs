48f1a392d936de2cfb8658811c5c812fbcb374b3
lemon-lime-honey <bs0308@g.skku.edu>
2024-11-01 11:30:05 +0900

    :sparkles: add example 3-6

---
+++ 03/ex3-6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using OpenCvSharp;
+
+namespace ex3_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Range range = new Range(0, 100);
+            Console.WriteLine($"{range.Start}, {range.End}");
+        }
+    }
+}
